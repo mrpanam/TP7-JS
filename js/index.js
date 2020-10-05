0d0fb43c317ded7c7fd7730eaf5abbdf9f7e1506
mrpan <mrpanam.p@gmail.com>
2020-10-05 11:36:41 +0200

    tp7-js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let img=document.getElementById("image");
+function affichedate(){
+    document.getElementById('demo').innerHTML = Date();
+}
+
+function repimage(){
+
+    img.src="images/2325234-bigthumbnail.jpg";
+}
