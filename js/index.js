551a550ba18bc3edc58a2cf373ecf94b5741435b
mrpan <mrpanam.p@gmail.com>
2020-10-05 14:35:15 +0200

    tp7-js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,27 @@
 let img=document.getElementById("image");
+const url = "http://graven.yt/citations.json";
 function affichedate(){
-    document.getElementById('demo').innerHTML = Date();
+    //document.getElementById('demo').innerHTML = Date();
+    $( "#demo" ).append(Date);
+
 }
 
 function repimage(){
-
     img.src="images/2325234-bigthumbnail.jpg";
 }
+function getjson() {
+    fetch(url).then((data) => {
+        data.json().then((data) => {
+            citations = data;
+            // affichage de citations
+            console.log(citations);
+            $.each(data, function (key, value) {
+
+
+            });
+
+        });
+    });
+}
+
+
